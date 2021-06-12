d34e106874137d72ca280bfd6fdd3538508b0f24
Yunge Yu <46149323+xcloudyunx@users.noreply.github.com>
2021-06-12 15:17:08 +1200

    Add files via upload

---
+++ Resources/Map/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileset" tilewidth="128" tileheight="128" tilecount="8" columns="8">
+ <image source="tileset.png" width="1024" height="128"/>
+</tileset>
